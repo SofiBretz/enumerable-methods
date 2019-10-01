b2ded7d36ca2cdad0bf2601850b86719913b335b
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-10-01 13:39:27 -0300

    added map and inject, test in process

--- enumerable-method.rb
+++ enumerable-method.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 module Enumerable
-
   # my_each
   def my_each
-
-    for i in 0...self.length
-        yield self[i]
+    (0...length).each do |i|
+      yield self[i]
     end
     # Analysis:
     # Self represent the current object, returns the elements of the array
@@ -15,99 +13,116 @@ module Enumerable
 
   # my_each_with_index
   def my_each_with_index
-
-    for i in 0...self.length
-        yield self[i], i
+    (0...length).each do |i|
+      yield self[i], i
     end
   end
 
   # my_select
   def my_select
-
-    #new array
+    # new array
     select_arr = []
 
     my_each do |i|
-        select_arr << i if yield(i)
+      select_arr << i if yield(i)
     end
     select_arr
-    #select: Returns a new array containing all elements of enum for which the given block returns a true value.
+      # select: Returns a new array containing all elements of enum for which the given block returns a true value.
     end
 end
 
-#my_all?
+# my_all?
 def my_all?
-    #all: The method returns true if ALL elements are true (or empty array). 
-    n = self.length
+  # all: The method returns true if ALL elements are true (or empty array).
+  n = length
 
-    select_arr = []
+  select_arr = []
 
-    self.my_each do |i|
-        if yield(i)
-            select_arr << i
-        end
-    end
+  my_each do |i|
+    select_arr << i if yield(i)
+  end
 
- return select_arr.length == n
+  select_arr.length == n
 end
 
-#my_any?
+# my_any?
 def my_any?
-    #any: The method returns true if AT LEAST one element is true (or non empty array).
+  # any: The method returns true if AT LEAST one element is true (or non empty array).
 
-    select_arr = self.my_select do |i|
-           yield(i)
-       end
+  select_arr = my_select do |i|
+    yield(i)
+  end
 
-    return select_arr.length > 0
+  !select_arr.empty?
 end
 
-    #my_none?
-    def my_none?
-        #none: The method returns true if NO elements are true (or empty array). 
+# my_none?
+def my_none?
+  # none: The method returns true if NO elements are true (or empty array).
 
-        select_arr = self.my_select {|i| yield(i)}
-        return !(select_arr.length > 0)
-    end
+  length.times do |i|
+    return false if yield self[i]
+  end
+  true
+      end
 
-     #my_count
-     def my_count(x)
-        x= nil
-        #count: The count method takes an enumerable collection and counts how many elements match the given criteria.
+# my_count
+def my_count
+  # count: The count method takes an enumerable collection and counts how many elements match the given criteria.
+  count = 0
 
-        if !block_given? && x == nil
-            select_arr = []
+  my_each do |element|
+    count += 1 if !block_given? || yield(element)
+  end
+  count
+      end
 
-            self.my_each do |i|
-                select_arr << i
-            end
-            return select_arr.length
-        end
+# my_map
+def my_map(proc = nil)
+  result = []
 
-        if !block_given? && x != nil
+  my_each do |element|
+    result << (block_given? ? yield(element) : proc.call(element))
+  end
+  result
+  end
 
-            select_arr = []
+# my_inject
+def my_inject(initial = nil)
+  result = initial
 
-            self.my_each do |i|
-                if i == x
-                    select_arr << i
-                end
-            end
-            return select_arr.length
-        end
+  my_each do |element|
+    result = if result.nil?
+               element
+             else
+               yield(result, element)
+             end
+  end
+  result
+  end
 
-        if block_given? && x == nil
-            select_arr = self.my_select {|i| yield(i)}
+class Array
+  include Enumerable
+end
 
-            return select_arr.length
-        end
-    end
+def multiply_els(arr)
+  arr.my_inject { |sum, x| sum * x }
+end
 
+# Tests:
+array = [1, 2, 3, 4]
 
-    #to test
-array = [1,2,3,4]
-#begin my_each 1 working
-array.each { |x| puts x +1}
-puts ''
-array.my_each { |x| puts x +1}
-#end
+# my_each
+# array.each { |x| puts x +1}
+# puts ''
+# array.my_each { |x| puts x +1}
+
+# my_each_with_index
+# array.each_with_index {|number, index| puts array [number] = index}
+# puts ""
+# array.my_each_with_index {|number, index| puts array [number] = index}
+
+# my_select
+# array.select {|element| puts element.even? }
+# puts ""
+# array.my_select {|element| puts element.even? }
