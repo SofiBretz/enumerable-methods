637bf8e7686e2b4393eaeb34b8c2e01e791b5dcc
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-10-03 14:50:04 -0300

    linters fixed, no offenses detected

--- enumerable_method.rb
+++ enumerable_method.rb
@@ -70,99 +70,52 @@ module Enumerable
       my_each { |x| count += 1 if x == items }
     end
     count
+  end
+
+  # my_map
+  def my_map(proc = nil)
+    return to_enum(:my_map) unless block_given?
+
+    new_arr = []
+    my_each do |p|
+      new_arr << if !proc.nil?
+                   proc.call(p)
+                 else
+                   yield(p)
+                 end
+    end
+    new_arr
+  end
+
+  # my_inject
+  def my_inject(*args)
+    arr = to_a.dup
+    if args[0].nil?
+      memo = arr.shift
+    elsif args[1].nil? && !block_given?
+      symbol = args[0]
+      memo = arr.shift
+    elsif args[1].nil? && block_given?
+      memo = args[0]
+    else
+      memo = args[0]
+      symbol = args[1]
     end
 
-  c = 0
-  n = 0
-  while c < size
-    n += 1 if yield(self[c])
-    c += 1
+    arr[0..-1].my_each do |elem|
+      memo = if symbol
+               memo.send(symbol, elem)
+             else
+               yield(memo, elem)
+             end
+    end
+    memo
   end
 
-  return n
+  def multiply_els(arr)
+    result = arr.my_inject do |mul, num|
+      mul * num
+    end
+    result
   end
-
-# my_map
-def my_map(proc = nil)
-  return to_enum(:my_map) unless block_given?
-
-  new_arr = []
-  my_each do |p|
-    new_arr << if !proc.nil?
-                 proc.call(p)
-               else
-                 yield(p)
-               end
-  end
-  new_arr
 end
-
-# my_inject
-def my_inject(*args)
-  arr = to_a.dup
-  if args[0].nil?
-    memo = arr.shift
-  elsif args[1].nil? && !block_given?
-    symbol = args[0]
-    memo = arr.shift
-  elsif args[1].nil? && block_given?
-    memo = args[0]
-  else
-    memo = args[0]
-    symbol = args[1]
-  end
-
-  arr[0..-1].my_each do |elem|
-    memo = if symbol
-             memo.send(symbol, elem)
-           else
-             yield(memo, elem)
-           end
-  end
-  memo
-end
-
-def multiply_els(arr)
-  result = arr.my_inject do |mul, num|
-    mul * num
-  end
-  result
-end
-end 
-
-# rubocop:enable
-# Tests:
-arr = [1, 3, 5, 9, 7]
-# # my_each
-# arr.my_each { |element| puts element }
-#
-# my_each_with_index
-# arr.my_each_with_index { |element, index| puts "#{index} -> #{element}" }
-#
-# my_select
-# puts(arr.my_select { |element| element > 5 })
-#
-# my_all?
-# puts(arr.my_all? { |element| element < 10 })
-#
-# my_any?
-# puts(arr.my_any? { |element| element == 1 })
-# my_none?
-puts(arr.my_none? { |element| element == 10 })
-
-# my_count
-puts(arr.my_count { |element| element == 9 })
-puts arr.my_count
-
-# my_inject
-puts(arr.my_inject { |sum, element| sum + element })
-
-# multiply_els([2,4,5])
-puts arr.multiply_els([2, 4, 5])
-
-# my_map (proc)
-proc = proc { |element| element * 10 }
-puts arr.my_map(proc)
-
-# my_map (block)
-puts(arr.my_map { |element| element * 10 })
